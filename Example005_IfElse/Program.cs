ca47ce16afcb944f7c03791de5d38ea61bced5e6
Ig2660 <110650490+Ig2660@users.noreply.github.com>
2022-11-29 06:12:27 +0300

    If Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "igor")
+{
+    Console.WriteLine("Hello, Igor!");
+}
+else
+{
+    Console.Write("Hy, ");
+    Console.WriteLine(username);
+}
